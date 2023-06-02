b89fc6b76ee4325bc6a292cfed27993942caa230
nsmtechdev <nsmtechdev@gmail.com>
2023-06-02 14:17:45 -0300

    a2

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/nsmtechdev/desafio-go-bases/internal/tickets"
+)
+
+func main() {
+	total, err := tickets.GetTotalTickets("Brazil")
+}
---
+++ reader.go
@@ -0,0 +1,42 @@
+package reader
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func ReadFile(){
+var TicketList []X.ticket 
+
+rawData, er := os.ReadFile("./tickets.csv")
+if err != nil{
+	panic(err/Error())
+}
+
+err = json.Unmarshal(rawData, TicketList)
+if err != nil{
+	panic(err)
+}
+
+fmt.Println(TicketList)
+
+for _, row := range rawData {
+	ticketList = append(ticketList, X.{
+			ID: row[0],
+			Name: row[1], 
+			Mail: row[2],
+			Country: row[3],
+
+
+	})
+} 
+/*type Ticket struct {
+	ID      string
+	Name    string
+	Mail    string
+	Country string
+	Time    string
+	Price  string
+}*/
+}

5eb33deb313d8c8f180a49c00b52033d0050e0dc
nsmtechdev <nsmtechdev@gmail.com>
2023-06-04 18:16:55 -0300

    c3

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"vuelos/tickets"
-	"log"
+	//"log"
 	"vuelos/reader"
 	
 	
@@ -11,31 +11,56 @@ import (
 
 func main() {
 
+/////////////////////////
+ch := make(chan string)
+
+
+
 	 reader.ReaderTickets()
+	///////////////////////////////////////
 	
-	
-	totalTicketes, err := tickets.GetTotalTickets("Brazil")
+	/*go totalTicketes, err := tickets.GetTotalTickets(ch, "Brazil")
 	 if err != nil {
 		log.Fatal("Vuelo de LAPA")
-	}
-	fmt.Println("el total de viajes al pais es", totalTicketes)	   
+	}*/
+
+	//go tickets.GetTotalTickets(ch, "Brazil")
 	
-	totalTicketes1, err := tickets.GetTotalPorDia("Brazil")
+/*
+	fmt.Println("el total de viajes al pais es", totalTicketes)	   
+	//////////////////////////////
+	
+	 totalTicketes1, err := tickets.GetTotalPorDia("Brazil")
 	if err != nil {
 	   log.Fatal("Vuelo de LAPA")
    }
+ 
    fmt.Println("el total de viajes al pais es", totalTicketes1)
+*/
 
-    
+
+// go tickets.GetTotalPorDia(ch, "Brazil")
+   	
+ 
+//////////////////////////////////////////////////
+    /*
    totalTime, err := tickets.GetCountbyPeriod("maniana")
    if err != nil {
 	  log.Fatal("error en el horario")
    }
   fmt.Println("el total de viajes en la fraja horario es", totalTime)
+ */
+ /*
+  fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+*/
+tickets.GetCountbyPeriod(ch, "noite")
  
-  
-
-  } 
+fmt.Println(<-ch)
+//fmt.Println(<-ch)
+//fmt.Println(<-ch)
+} 
    
 
 
--- tickets/tickets.go
+++ tickets/tickets.go
@@ -9,19 +9,16 @@ import (
 	"vuelos/reader"
 	//"errors"
 )
+///////////////////////////////////Ej1
 
-
-func GetTotalTickets(id string) (int, error) {
+func GetTotalTickets(ch chan string , id string) (int, error) {
 	
 	rawData, err := os.ReadFile("./tickets.csv")
 	if err != nil {
-		log.Fatal(err)
-	}
-	 
+		log.Fatal(err)}
 	data := strings.Split(string(rawData), "\n")
-
-
 //fmt.Println(data)
+
 	var total float64
 	for i, v := range data {
 		linea := strings.Split(v, ",")
@@ -29,33 +26,33 @@ func GetTotalTickets(id string) (int, error) {
 			if linea[3] != id{
 				if err != nil {
 					fmt.Println("Debe ser un vuelo de LAPA")
-			continue
+				continue
 			}}
-				if linea[3] == id {
-					total += 1
+			if linea[3] == id {
+					total += 1}		
+				}
+	}
+		if total == 0 {
+			ch <- fmt.Sprintf("sin pasajeros pa el vuelo: %s", id)}
+		
+			if total >= 1 {
+		ch <- fmt.Sprintf("hay %f pasajeros en destino a %s", total, id)}
 
-		}
-}
-	
-}
 
 return int(total), nil
 
 }
 
-///////////////////////////////////////////////////////////////////////
-func GetTotalPorDia(id string) (int, error) {
+/////////////////////////////////////////////////////////////////////// Ej3
+func GetTotalPorDia(ch chan string, id string) (int, error) {
 		
 	rawData, err := os.ReadFile("./tickets.csv")
 	if err != nil {
-		log.Fatal(err)
-	}
-	 
+		log.Fatal(err)	}
 	data := strings.Split(string(rawData), "\n")
-
-
 //fmt.Println(data)
-	var total float64
+
+var total float64
 	//var ListaTotal []reader.Ticket
 	for i, v := range data {
 		linea := strings.Split(v, ",")
@@ -67,23 +64,15 @@ func GetTotalPorDia(id string) (int, error) {
 			}}
 				if linea[3] == id {
 					total += 1}
-
-				
-			}
-	
-}
-
-
+						}
+	}
 var	porcentaje = (float64(total) * 100.0 )/ float64(len(data))
+ch <- fmt.Sprintf("el porcentaje de vuelos a %s es de %.2f", id, porcentaje)
 
 return int(porcentaje), nil
 }
 
-
-
-
-
-//////////////////////////////////
+//////////////////////////////////Ej2
 const (
 	madrugada = "madrugada"
 	maniana= "maniana"
@@ -91,9 +80,10 @@ const (
 	noite = "noite"
 )
 
-func GetCountbyPeriod(tipo string ) (int, error) {
+func GetCountbyPeriod(ch chan string, tipo string ) (int, error) {
 	var totalTime int
 	switch tipo {
+	
 	case madrugada:
 			
 		for _, ticket := range reader.ListaDeTicketes {
@@ -101,12 +91,9 @@ func GetCountbyPeriod(tipo string ) (int, error) {
 			if err != nil {
 			panic("no pude splitear o transfomrar a int ")}
 				if horaM >= 0 && horaM <= 6  {
-					totalTime += 1}			
-	}
-		
+					totalTime += 1}		}	
 	return int(totalTime), nil
 	
-
 	case maniana:
 			
 	for _, ticket := range reader.ListaDeTicketes {
@@ -114,8 +101,7 @@ func GetCountbyPeriod(tipo string ) (int, error) {
 		if err != nil {
 		panic("no pude splitear o transfomrar a int ")}
 			if horaM >= 7 && horaM <= 12  {
-				totalTime += 1}			
-}
+				totalTime += 1}		}
 return int(totalTime), nil
 
 	case tarde:
@@ -125,9 +111,7 @@ return int(totalTime), nil
 		if err != nil {
 		panic("no pude splitear o transfomrar a int ")}
 			if horaM >= 13 && horaM <= 19  {
-				totalTime += 1}			
-}
-	
+				totalTime += 1}		}
 return int(totalTime), nil
 
 	case noite:
@@ -137,9 +121,10 @@ return int(totalTime), nil
 		if err != nil {
 		panic("no pude splitear o transfomrar a int ")}
 			if horaM >= 20 && horaM <= 23  {
-				totalTime += 1}			
-		}
+				totalTime += 1}		}
+	return int(totalTime), nil
 	}
+	ch <- fmt.Sprintf("hay  %d pasajeros en el periodo  %s", totalTime, tipo)
 return int(totalTime), nil
 
 }
